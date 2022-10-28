e156ab2084808eb4dc7acfa4b13fa84ccc66423d
zachpren0 <77454945+zachpren0@users.noreply.github.com>
2022-10-28 10:11:17 -0700

    Update App.js

--- MERN Scripts/mern-emsystem/src/App.js
+++ MERN Scripts/mern-emsystem/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="Rotary-logo" alt="logo"/>
         <p>
-          This is our logo
+          This is our logo -still in dev
         </p>
         </header>
       </div>
