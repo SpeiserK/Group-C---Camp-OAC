6f1506d77a0f212db8a28aba1f3592c65dd18db9
camwlsn <cam.wilson10@gmail.com>
2022-11-17 14:00:44 -0800

    Order page styling
    App.css: Added styling to multiple new classes
    matchEmail.js: Added className to two features
    placeOrder.js: Added className to three features
    Orderqty.js: Added logo to order page

--- MERN Scripts/mern-emsystem/src/pages/Orderqty.js
+++ MERN Scripts/mern-emsystem/src/pages/Orderqty.js
@@ -12,10 +12,10 @@ function Orderqty(){
     return( 
         <div>
             <div className="main">
-
+  
 
             <Banner />
-
+            <Logo />
             <PlaceOrder />
             <Link to="/">Click here to go back</Link><br></br>
             <Link to="payment">Click here to view Payment page</Link>
