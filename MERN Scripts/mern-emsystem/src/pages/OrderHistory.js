be7cbec5610d7af0d7d0104cb018f37a2d09fd70
amurph24 <amurph@student.ubc.ca>
2022-11-23 12:31:32 -0800

    added data to order history page

--- MERN Scripts/mern-emsystem/src/pages/OrderHistory.js
+++ MERN Scripts/mern-emsystem/src/pages/OrderHistory.js
@@ -1,15 +1,49 @@
 import React from 'react'
 import Navbar from '../components/Navbar.js';
 import Banner from '../components/Banner.js';
+import axios from "axios";
+
+const {useEffect, useState} = React;
+const fetchData = async () => {
+    return axios.get('http://localhost:5000/mongo')
+    .then( ({data}) => {
+        console.log(data);
+        return JSON.stringify(data);
+
+    })
+    .catch( err => {
+
+        console.error(err);
+    });
+
+}
+
+
 
 
 function OrderHistory(){
+    const [OrderData, setOrderData] = useState('');
+
+    useEffect(() => {
+        fetchData().then(Data => {
+            setOrderData(Data || ' ');
+
+        })
+        
+    });
+
     return(
         <div className="main">
             <Banner />
             <div className="container">
                 
                 <p>Order History.</p>
+
+
+
+
+
+            {OrderData}
             </div>
             
             <div className="footer">
