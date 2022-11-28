977cc8065cb827077472178e7aeb640c78777f9c
amurph24 <amurph@student.ubc.ca>
2022-11-27 16:10:26 -0800

    server check on banner

--- MERN Scripts/mern-emsystem/src/components/Navbar.js
+++ MERN Scripts/mern-emsystem/src/components/Navbar.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { useState } from 'react';
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import { text } from '@fortawesome/fontawesome-svg-core';
 
 export default class Navbar extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+          count: 0,
+          online: ''
+        };
+      }
+
+    
+      componentDidMount(){
+        axios.get(`http://localhost:5000/`)
+      .then(res => {
+        const data = res.data;
+        this.setState({ online: data });
+      })
+      }
+
+
     render(){
+
+
+
         return(
             <div className="mainNav">
+                {this.state.online.message}
                 <button style={{textDecoration: 'none' , color: 'black'}}>
                 <Link to="/login" style={{textDecoration: 'none' }}>Admin Portal</Link>
                 </button>
