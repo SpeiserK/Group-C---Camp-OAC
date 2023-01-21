bd324ae3ead1096a83c1fa089cfc838a77fad286
Karlen Speiser <karlenspeiser@gmail.com>
2023-01-20 21:26:02 -0800

    status now changes to approved if payment type is credit/debit

--- MERN Scripts/mern-emsystem/src/components/OrderDispHist.js
+++ MERN Scripts/mern-emsystem/src/components/OrderDispHist.js
@@ -2,15 +2,19 @@ import React from 'react';
 import axios from 'axios';
 
 export default class OrderDispHist extends React.Component {
-  state = {
-    orders: []
+  constructor(props){
+    super(props);
+    this.state = {
+      orders: []
+    }
   }
+  
 
   componentDidMount() {
     axios.get(`http://localhost:5000/order`)
       .then(res => {
-        const orders = res.data;
-        this.setState({ orders });
+        const orderData = res.data;
+        this.setState({ orders: orderData });
       })
   }
 
@@ -18,25 +22,61 @@ export default class OrderDispHist extends React.Component {
   
   render() {
     return (
-      
-      <ul className="dblist">
-        <h1> Order history </h1>
+        <table className="dblist">
+        <tr id="listHeader" className="listHeader">
+          <td id="headerID"  className="listHeaderItem">
+            <span> Order id </span>
+          </td>
+          <td id="headerEmail"  className="listHeaderItem">
+            <span> Email </span>
+          </td>
+          <td id="headerQuantity" className="listHeaderItem">
+            <span> Quantity </span>
+          </td>
+          <td id="headerLocation" className="listHeaderItem">
+            <span> Location </span>
+          </td>
+          <td id="headerDate" className="listHeaderItem">
+            <span> Date </span>
+          </td>
+          <td id="headerPrice" className="listHeaderItem">
+            <span> Payment Details</span>
+          </td>
+          <td id="headerStatus" className="listHeaderItem">
+            <span> Status </span>
+          </td>
+        </tr> 
+
         {
           this.state.orders
-            .map(content =>
-              <li key={content._id}>
-                <span>Email: {content.Name}</span>&emsp;
-                <span>Quantity Ordered: {content.Quantity}</span>&emsp;
-                <span>Location: {content.Location}</span>&emsp;
-                <span>Date/Time: {content.Datetime}</span>&emsp;
-                <span>Price: ${content.Price}</span>&emsp;
-                <span>Payment: {content.Payment}</span>&emsp;
-                <span>Status: {content.Status}</span>&emsp;
-                <span>Picked up: {content.Pickup}</span>&emsp;
-              </li>
+            .map((content,index) =>
+            
+              <tr id="list-orderList" key={content._id} Name={`buttons-${index}`}>  
+                <td id="current-orderID" className ="orderChild">
+                  <span>{content._id}</span>&emsp;
+                </td>     
+                <td id="current-orderEmail" className ="orderChild">
+                    <span>{content.Name}</span>&emsp;
+                </td>
+                <td id="current-orderQty" className ="orderChild">
+                    <span>{content.Quantity}</span>&emsp;
+                </td>
+                <td id="current-orderLoc" className ="orderChild">
+                    <span>{content.Location}</span>&emsp;
+                </td>
+                <td id="current-orderDate" className ="orderChild">
+                  <span>{content.Datetime}</span>&emsp;
+                </td>
+                <td id="current-orderPrice" className ="orderChild">
+                  <span>${content.Price} &nbsp; {content.Payment}</span>&emsp;
+                </td>
+                <td id="current-approve-deny"className ="orderChild"> 
+                <span>{content.Status} &nbsp; Picked up: {content.Pickup}</span>&emsp;
+                </td>         
+              </tr>
             )
         }
-      </ul>
+      </table>
     )
   }
 }
