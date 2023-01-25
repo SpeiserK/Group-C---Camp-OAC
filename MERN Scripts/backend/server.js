6540bcb1472c5ccc6e56aa9ba5108b565fd9e01e
Karlen Speiser <karlenspeiser@gmail.com>
2023-01-25 15:01:44 -0800

    emails are now sent to dev email for orders approved from cash and e-transfer payments

--- MERN Scripts/backend/server.js
+++ MERN Scripts/backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const nodeMailer = require('nodemailer');
 require('dotenv').config();
 const Models = require("./models.js");
 
@@ -89,10 +90,40 @@ app.post("/locupdate", (req, res) => {
     });
 });
 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Nodemailer functions
+
+let transporter = nodeMailer.createTransport({
+    service: "gmail",
+    auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL,
+        pass: process.env.WORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    },
+});
+
+transporter.verify((err, success) =>{
+    err
+        ? console.log(err)
+        : console.log(`== Server is  ready to take messages: ${success} ===`)
+});
+
+
+//status change post request
 app.post("/statuschange", (req, res) => {
     const idS = req.body.id;
     const status = req.body.status;
     const pickup = req.body.pickup;
+    const quantity = req.body.quantity;
+    const location = req.body.location;
+    const date = req.body.date;
+    const price = req.body.price;
+    const email = req.body.email;
 
     if (!idS||!status || !pickup){
         return res.status(422).json({error:"Missing Fields"})
@@ -102,13 +133,28 @@ app.post("/statuschange", (req, res) => {
         if(err) return console.log(err);
     });
 
+    if(status == "Approved"){
+        let mailOptions = {
+            from: process.env.EMAIL,
+            to: process.env.EMAIL,          //will replace with email const
+            subject: "Your firewood order has been approved.",
+            text: "Hello "+email+", your order for "+quantity+" fire wood bundle(s) has been approved for $"+price+". Your pickup address will be "+location+", your order will be available for pickup until "+date+" CLOSING TIME (PST).",
+        };
+        
+        transporter.sendMail(mailOptions, function (err, data) {
+            if(err){
+                console.log("Error"+err);
+            } else{
+                console.log("Email sent successfully");
+            }
+        });
+    }
 });
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.use("/", require("./routes/OrderRoute.js"));
 //emp creation
 app.use("/api/emp", require("./routes/EmpRoute.js"));
 //emp auth
 app.use("/api/auth", require("./routes/Auth.js"));
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
