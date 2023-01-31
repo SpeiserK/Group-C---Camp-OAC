4c9f3c0bf33c44a91de538bd5a888a445f60b14d
amurph24 <amurph@student.ubc.ca>
2023-01-30 16:07:24 -0800

    add superadmin account creation

--- MERN Scripts/mern-emsystem/src/App.js
+++ MERN Scripts/mern-emsystem/src/App.js
@@ -14,7 +14,7 @@ import CashConfirmation from './pages/CashConfirmation';
 import ETransferConfirmation from './pages/ETransferConfirmation';
 import SquareConfirmation from './pages/SquareConfirmation';
 import SquarePay from './pages/SquarePay';
-import EmployeeList from './pages/EmployeeList';
+import SuperAdmin from './pages/SuperAdmin';
 
 function App() {
   return (
@@ -34,7 +34,7 @@ function App() {
           <Route path="login/emp/viewOrders" element={<ViewOrders/>} />
           <Route path="login/emp/inventory" element={<Inventory/>} />
           <Route path="login/emp/history" element={<OrderHistory/>} />
-          <Route path="login/emp/employee" element={<EmployeeList/>} />
+          <Route path="login/emp/employee" element={<SuperAdmin/>} />
         </Routes>
         
       </div>
