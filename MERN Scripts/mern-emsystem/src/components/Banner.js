6a8a76acef44c076456e89f0d678e32e80487f79
Karlen Speiser <karlenspeiser@gmail.com>
2023-03-03 17:30:44 -0800

    make banner resize better w dropdown

--- MERN Scripts/mern-emsystem/src/components/Banner.js
+++ MERN Scripts/mern-emsystem/src/components/Banner.js
@@ -5,16 +5,24 @@ import Logo2 from './Logo2.js';
 import Drop from './Dropdown.js';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/esm/Button';
 
 export default class Banner extends Component {
     render(){
         return(
             <Container fluid>
                     <div className="bHeader">
-                    <Col lg={3} sm={2} className="d-none d-lg-block d-md-block"><Logo2 fluid /></Col>
-                    <Col><Link to="/" className="Link" style={{textDecoration: 'none', padding: 20 }}><h1 className="bearHug">rotary club of kelowna ogopogo</h1></Link> </Col>
-                    <Col lg={1}><Drop/></Col>
-                    <Col xl={{span:2, offset:0}} lg={{span:2,offset:0}}  md={{span:1,offset:0}} sm={{span:1,offset:0}} className="d-none d-lg-block d-md-block">
+                    <Col xl={{span:1,offset:0}} lg={1} className="d-none d-lg-block d-md-block"><Logo2 fluid /></Col>
+                    <Col xl={{span: 5, offset:0}} lg={{span: 7, offset:1}} md={{span: 7, offset:1}} ><Link to="/" className="Link" style={{textDecoration: 'none', padding: 20 }}><h1 className="bearHug">rotary club of kelowna ogopogo</h1></Link> </Col>
+                    <Col lg={{span:1, offset: 0}} md={{span:1, offset: 0}} sm={{span:1, offset: 3}} xs={{span:1, offset: 2}} className="d-xl-none d-block"><Drop/></Col>
+                    <Col xl={{span:5,offset:0}} lg={{span: 5, offset:4}} className="d-none d-xl-block" id="headerButton">
+                        <Button variant="outline-light" size="sm" href="/about">About</Button>{' '}
+                        <Button variant="outline-light" size="sm" href="https://www.facebook.com/ogopogorotary/" target="_blank">Facebook</Button>{' '}
+                        <Button variant="outline-light" size="sm" href="https://portal.clubrunner.ca/824" target="_blank">Kelowna Rotary</Button>{' '}
+                        <Button variant="outline-light" size="sm" href="https://portal.clubrunner.ca/824" target="_blank">Camp OAC</Button>{' '}
+                        <Button variant="outline-light" size="sm" href="/login">Admin Portal</Button>{' '}
+                    </Col>
+                    <Col xl={{span:1, offset:0}} lg={{span:1,offset:0}}  md={{span:1,offset:0}} className="d-none d-lg-block d-md-block">
                         <div className="bHeadicons">
                             <Logo fluid/>
                          </div>
