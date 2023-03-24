cf15014ec1aa53093d26c41533432e76158fbc05
Karlen Speiser <karlenspeiser@gmail.com>
2023-03-24 13:08:42 -0700

    add tooltip to order image

--- MERN Scripts/mern-emsystem/src/components/Bundles2.js
+++ MERN Scripts/mern-emsystem/src/components/Bundles2.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import Bundle2 from './/pictures/Bundle2.jpg';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 export default class Bundles2 extends Component {
     render(){
+        const renderBundletip = (props) => (
+            <Tooltip id="hover-tooltip"{...props}>
+             Bundle Size Reference
+            </Tooltip>
+        );
         return(
             <div className="bundles2">
-                <img alt="bundles2img" src={Bundle2} />
+                <OverlayTrigger
+                placement="auto"
+                delay={{show:250,hide:400}}
+                overlay={renderBundletip}
+                >
+                    <img alt="bundles2img" src={Bundle2} />
+                </OverlayTrigger>
             </div>
         );            
     }
